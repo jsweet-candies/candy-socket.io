63077591a4b273ad43d67f345a2bff709517e986
Louis Grignon <louis.grignon@gmail.com>
2017-04-26 09:50:40 +0200

    $apply

--- src/main/java/def/socket_io/SocketIOStatic.java
+++ src/main/java/def/socket_io/SocketIOStatic.java
@@ -4,24 +4,24 @@ public abstract class SocketIOStatic extends def.js.Object {
     /**
 	 * Default Server constructor
 	 */
-    native public def.socket_io.socketio.Server apply();
+    native public def.socket_io.socketio.Server $apply();
     /**
 	 * Creates a new Server
 	 * @param srv The HTTP server that we're going to bind to
 	 * @param opts An optional parameters object
 	 */
-    native public def.socket_io.socketio.Server apply(Object srv, def.socket_io.socketio.ServerOptions opts);
+    native public def.socket_io.socketio.Server $apply(Object srv, def.socket_io.socketio.ServerOptions opts);
     /**
 	 * Creates a new Server
 	 * @param port A port to bind to, as a number, or a string
 	 * @param An optional parameters object
 	 */
-    native public def.socket_io.socketio.Server apply(String port, def.socket_io.socketio.ServerOptions opts);
+    native public def.socket_io.socketio.Server $apply(String port, def.socket_io.socketio.ServerOptions opts);
     /**
 	 * Creates a new Server
 	 * @param A parameters object
 	 */
-    native public def.socket_io.socketio.Server apply(def.socket_io.socketio.ServerOptions opts);
+    native public def.socket_io.socketio.Server $apply(def.socket_io.socketio.ServerOptions opts);
     /**
 	 * Backwards compatibility
 	 * @see io().listen()
@@ -32,25 +32,25 @@ public abstract class SocketIOStatic extends def.js.Object {
 	 * @param srv The HTTP server that we're going to bind to
 	 * @param opts An optional parameters object
 	 */
-    native public def.socket_io.socketio.Server apply(Object srv);
+    native public def.socket_io.socketio.Server $apply(Object srv);
     /**
 	 * Creates a new Server
 	 * @param port A port to bind to, as a number, or a string
 	 * @param An optional parameters object
 	 */
-    native public def.socket_io.socketio.Server apply(String port);
+    native public def.socket_io.socketio.Server $apply(String port);
     /**
 	 * Creates a new Server
 	 * @param port A port to bind to, as a number, or a string
 	 * @param An optional parameters object
 	 */
-    native public def.socket_io.socketio.Server apply(double port, def.socket_io.socketio.ServerOptions opts);
+    native public def.socket_io.socketio.Server $apply(double port, def.socket_io.socketio.ServerOptions opts);
     /**
 	 * Creates a new Server
 	 * @param port A port to bind to, as a number, or a string
 	 * @param An optional parameters object
 	 */
-    native public def.socket_io.socketio.Server apply(double port);
+    native public def.socket_io.socketio.Server $apply(double port);
     /**
 	 * Default Server constructor
 	 */
